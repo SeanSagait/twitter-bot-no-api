4de53ba5ebbe8b13a2d105eeb31dc9d8d7ba313e
SeanSagait <127765284+SeanSagait@users.noreply.github.com>
2023-07-10 01:35:24 -0400

    Refactored code to use undetected_chromedriver and added functionality to get top crypto posts and generate comments using GPT-3.5 Turbo.y

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-from selenium import webdriver
+import undetected_chromedriver.v2 as uc
+import openai
+import random
+import sys
+from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 from time import sleep
-
 from dotenv import dotenv_values
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
+
+# Initialize OpenAI API 
+config = dotenv_values('.env')
+
+openai.api_key = config["OPENAI_API_KEY"]
 
 class Twitter:
-    def __init__(self, email:str, password:str, username:str) -> None:       
-        self.driver =  webdriver.Chrome()
+    def __init__(self, email:str, password:str, username:str):       
+        self.driver = uc.Chrome ()
         self.driver.get("https://twitter.com/login")
         self.email = email
         self.password = password
@@ -45,17 +55,86 @@ class Twitter:
         )
         element.send_keys(self.username)
         element.send_keys(Keys.ENTER)
-        
-    def tweet(self, text:str):
+        sleep(5)
+
+    def tweet(self, text: str):
+        print("Tweeting...")
+        reply_button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[@data-testid='reply']/parent::div"))
+        )
+        reply_button.click()
+
+        print("Clicked on reply button.")
+        sleep(4)
+        element = self.driver.find_element(By.XPATH, '//div[@aria-label="Tweet text"]')
+        print("Located the tweet text input field.")
         sleep(3)
-        self.driver.find_element(By.CLASS_NAME, 'DraftEditor-root').click()
-        element=self.driver.find_element(By.CLASS_NAME, 'public-DraftEditorPlaceholder-root')
         ActionChains(self.driver).move_to_element(element).send_keys(text).perform()
-        sleep(3)
-        self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div[3]").click()
+        print("Entered tweet text.")
+        sleep(10)
+        tweet_button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[@data-testid='tweetButton']//span[contains(@class, 'css-')]"))
+        )
+        self.driver.execute_script("arguments[0].click();", tweet_button)
+        sleep(30)
+        print("Clicked on tweet button.")
+        print("Tweeted successfully!")
+
+
+    def get_top_crypto_posts(self, cryptocurrency:str):
+    # Navigate to Twitter's search results page for the given cryptocurrency
+        self.driver.get(f"https://twitter.com/search?q={cryptocurrency}&src=typed_query&f=top")
+        sleep(10)
+
+        # Extract the tweet elements from the page
+        tweet_elements = self.driver.find_elements(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/section/div/div/div[2]')
+
+        # Extract the text of each tweet and add it to the list of posts
+        posts = []
+        for tweet_element in tweet_elements:
+            post = tweet_element.text
+            posts.append(post)
+
+        return posts
+
+    def generate_comment(self, post):
+        prompt = f"Generate a relevant and engaging comment about this top crypto post: '{post}'"
         
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ],
+        )
+        
+        return response['choices'][0]['message']['content'].strip()
+
 if __name__ == "__main__":
-    config = dotenv_values('.env')
     twitter = Twitter(config['LOGIN'], config["PASSWORD"], config["USERNAME"])
     twitter.login()
-    twitter.tweet("hello world")
+    sleep(5)
+        
+        # Scrape trending crypto posts
+    top_crypto_posts = twitter.get_top_crypto_posts('crypto news')
+    print(f"Top crypto posts: {top_crypto_posts}")
+
+    if not top_crypto_posts:
+        print("No top crypto posts found. Exiting.")
+        input("Press Enter to close the browser window...")
+        twitter.driver.quit()
+        sys.exit(1)
+
+    sleep(5)
+    
+    # Choose a random post from the list of trending posts
+    selected_post = random.choice(top_crypto_posts)
+    print(f"Selected post: {selected_post}")
+
+    # Generate a comment using GPT-3.5 Turbo
+    generated_comment = twitter.generate_comment(selected_post)
+    print(f"Generated comment: {generated_comment}")
+
+    # Tweet the generated comment
+    twitter.tweet(generated_comment)
+
